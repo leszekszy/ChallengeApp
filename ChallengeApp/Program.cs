c1066c008e6a1c370f241a44041312bdf6dc7d73
Leszek Szymczyk <leszek.szymczyk@gmail.com>
2023-06-09 10:59:02 +0200

    Zadanie, Dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isWoman = true;
+int age = 33;
+
+if (isWoman)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+
+    }
+}
