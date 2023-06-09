96b1b6cdb87823da6c3ad277297b67475732baa6
Leszek Szymczyk <leszek.szymczyk@gmail.com>
2023-06-09 20:16:24 +0200

    Dzień 5, zadanie domowe

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,27 +1,25 @@
-﻿var name = "Ewa";
-bool isWoman = true;
-int age = 33;
+﻿int number = 13777799;
+string numberInString = number.ToString();
+char[] numbersAsChars = numberInString.ToArray();
 
-if (isWoman)
+
+string[] result = { "0 => ", "1 => ", "2 => ", "3 => ", "4 => ", "5 => ", "6 => ", "7 => ", "8 => ", "9 => " };
+int[] result2 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+foreach (char c in numbersAsChars)
 {
-    if (age < 30)
+    for (int j = 0; j < 10; j++)
     {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-    else if (name == "Ewa" && age == 33)
-    {
-        Console.WriteLine("Ewa, lat 33");
+        if (j.ToString() == c.ToString())
+        {
+            result2[j] += 1;
+        }
     }
 }
-else
-{
-    if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni Mężczyzna");
-    }
-    else
-    {
-        Console.WriteLine("Pełnoletni Mężczyzna");
 
-    }
+Console.WriteLine("Wyniki dla liczby: " + number);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(result[i] + result2[i]);
 }
